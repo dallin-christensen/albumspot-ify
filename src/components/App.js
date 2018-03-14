58232e2282a3b2442863449c502768fd559fe409
Dallin <christensen.dallin@gmail.com>
2018-03-13 20:51:44 -0700

    create-react-app

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>Hellos</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/redux-schema.js
@@ -0,0 +1,14 @@
+{
+  tracks : { //all tracks
+    [trackId]: {
+      //track defining info, cover art, etc
+    }
+  },
+  user : {
+    //user info
+  },
+  playlist : {
+    upNext : [trackId, trackId]
+    played : [trackId]
+  }
+}
