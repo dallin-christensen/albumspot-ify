99e3ed314fce2492aea6893f7f47f0d25c1f2635
Dallin <christensen.dallin@gmail.com>
2018-04-12 06:26:23 -0700

    styles added to GameReview component

--- src/components/GameReview/GameReview.js
+++ src/components/GameReview/GameReview.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { PlaylistIcon } from '../'
 import { clearTracksAndArt } from '../../actions'
+import './style.css'
 
 class GameReview extends Component {
   toChoosePlaylist = () => {
@@ -10,14 +11,25 @@ class GameReview extends Component {
   render () {
     const { score, trackLen, playlistName, playlistImg } = this.props
     const percent = Math.round((score/trackLen) * 100)
+    const goodNews = percent >= 70 ? true : false
     return (
-      <div>
-        <div>{percent}%</div>
-        <PlaylistIcon
-          imgSrc={playlistImg}
-          name={playlistName}
-        />
-        <div onClick={this.toChoosePlaylist}>go back</div>
+      <div className='review_container'>
+        <div className='review_message'>
+          {goodNews ? 'You win! Now go get that master\'s!' : 'Bummer! Try again!'}
+        </div>
+        <div className='review_flex'>
+          <PlaylistIcon
+            imgSrc={playlistImg}
+            name={playlistName}
+          />
+          <div className='review_data_container'>
+            <div className='review_score'>{score}/{trackLen}</div>
+            <div className={goodNews ? 'review_percent' : 'review_percent red'}>{percent}%</div>
+            <div className='back_container'>
+              <button className='review_back' onClick={this.toChoosePlaylist}>To Playlists</button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
