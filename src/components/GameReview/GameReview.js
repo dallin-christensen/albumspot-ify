9cff35a4fe99d220c3762e1c263fe1f7d353419d
Dallin <christensen.dallin@gmail.com>
2018-04-12 20:43:01 -0700

    styles to GameReview component

--- src/components/GameReview/GameReview.js
+++ src/components/GameReview/GameReview.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { PlaylistIcon } from '../'
-import { clearTracksAndArt } from '../../actions'
+import { clearTracksAndArt, handleGetPlaylist } from '../../actions'
 import './style.css'
 
 class GameReview extends Component {
   toChoosePlaylist = () => {
     this.props.dispatch(clearTracksAndArt())
   }
+  replayPlaylist = () => {
+    const { dispatch, playlistHref } = this.props
+    dispatch(clearTracksAndArt())
+    dispatch(handleGetPlaylist(playlistHref))
+  }
   render () {
-    const { score, trackLen, playlistName, playlistImg } = this.props
+    const { score, trackLen, playlistName, playlistImg, playlistHref, dispatch } = this.props
     const percent = Math.round((score/trackLen) * 100)
     const goodNews = percent >= 70 ? true : false
     return (
       <div className='review_container'>
         <div className='review_message'>
-          {goodNews ? 'You win! Now go get that master\'s!' : 'Bummer! Try again!'}
+          {goodNews ? 'You win! Go brag to your friends!' : 'Bummer! Try again!'}
         </div>
         <div className='review_flex'>
-          <PlaylistIcon
-            imgSrc={playlistImg}
-            name={playlistName}
-          />
+          <span onClick={this.replayPlaylist}>
+            <PlaylistIcon
+              imgSrc={playlistImg}
+              name={playlistName}
+            />
+          </span>
           <div className='review_data_container'>
             <div className='review_score'>{score}/{trackLen}</div>
             <div className={goodNews ? 'review_percent' : 'review_percent red'}>{percent}%</div>
@@ -41,6 +48,7 @@ function mapStateToProps ({ game, tracks, playlist }) {
     trackLen: tracks.tracks.length,
     playlistName: playlist.name,
     playlistImg: playlist.images[0].url,
+    playlistHref: playlist.href,
   }
 }
 
